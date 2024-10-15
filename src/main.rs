7ca934bce43ff33020a8a6849191720a14213a16
Mohan Raj <mohanzolomon@gmail.com>
2024-10-15 14:47:24 +0530

    add structs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+#[derive(Debug)]
+struct Account {
+    id: usize,
+    balance: isize,
+    holder: String,
+}
+
+#[derive(Debug)]
+struct Bank {
+    accounts: Vec<Account>,
+}
+
 fn main() {
     println!("Hello, world!");
 }
